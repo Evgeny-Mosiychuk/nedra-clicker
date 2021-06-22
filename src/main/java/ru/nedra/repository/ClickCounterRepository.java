eecf42696824d301022a7147c503c7b9d4265ab9
Evgeny.Mosiychuk <evgeny.mosiychuk@gmail.com>
2021-06-23 00:23:35 +0300

    - added concurrency support

--- src/main/java/ru/nedra/repository/ClickCounterRepository.java
+++ src/main/java/ru/nedra/repository/ClickCounterRepository.java
@@ -1,9 +1,17 @@
 package ru.nedra.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 import ru.nedra.model.ClickCounter;
 
+import javax.persistence.LockModeType;
+import java.util.List;
+
 @Repository
 public interface ClickCounterRepository extends JpaRepository<ClickCounter, Integer> {
+
+    @Override
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    List<ClickCounter> findAll();
 }
--- src/main/java/ru/nedra/service/ClickCounterServiceImpl.java
+++ src/main/java/ru/nedra/service/ClickCounterServiceImpl.java
@@ -9,14 +9,14 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.Optional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class ClickCounterServiceImpl implements ClickCounterService {
 
     private final ClickCounterRepository repository;
 
     @Override
-    @Transactional
-    public void click() {
+    public void click(){
         Optional<ClickCounter> clickCounterOptional = repository.findAll().stream().findAny();
 
         if (clickCounterOptional.isEmpty()) {
@@ -25,7 +25,6 @@ public class ClickCounterServiceImpl implements ClickCounterService {
 
         ClickCounter clickCounter = clickCounterOptional.get();
         clickCounter.setClicksCount(clickCounter.getClicksCount() + 1);
-
         repository.save(clickCounter);
     }
 
